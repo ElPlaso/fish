35570d4a4cadd72eb987043414e05065c674ed2b
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-09-06 20:27:22 +1200

    Generate all positions and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,36 @@
-use rand::Rng;
+use std::fs::File;
+use std::io::prelude::*;
 
-const NUMBER_OF_BACK_RANK_PIECES: usize = 8;
+use itertools::Itertools;
 
-fn main() {
-    let mut black_back_rank: [char; NUMBER_OF_BACK_RANK_PIECES] = [' '; NUMBER_OF_BACK_RANK_PIECES];
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("chess960_positions.txt")?;
+
+    let black_pieces = vec!['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'];
     let inner_fen = "/pppppppp/8/8/8/8/PPPPPPPP/";
 
-    let black_pieces: [char; NUMBER_OF_BACK_RANK_PIECES] = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'];
+    for perm in black_pieces
+        .iter()
+        .permutations(black_pieces.len())
+        .unique()
+        .filter(|p| {
+            // Filter invalid chess960 positions, i.e king not inbetween rooks and bishops on same color
+            // Use of unwraps here as we know that these chars will be in the vec
+            let king_position = p.iter().position(|&&c| c == 'k').unwrap();
 
-    let mut rng = rand::rng();
+            p.iter().position(|&&c| c == 'r').unwrap() < king_position
+                && p.iter().rposition(|&&c| c == 'r').unwrap() > king_position
+                && p.iter().position(|&&c| c == 'b').unwrap() % 2
+                    != p.iter().rposition(|&&c| c == 'b').unwrap() % 2
+        })
+    {
+        let black_fen = String::from_iter(perm);
+        let white_fen = black_fen.as_str().to_uppercase();
 
-    for i in 0..NUMBER_OF_BACK_RANK_PIECES {
-        let mut available_pieces = black_pieces.to_vec();
+        let fen = black_fen + inner_fen + &white_fen;
 
-        available_pieces.retain(|p| {
-            black_back_rank
-                .iter()
-                .filter(|&&rank_char| rank_char == *p)
-                .count()
-                < 2
-        });
-
-        // King should be between 2 rooks, there should only be one king
-        if i == 0 || black_back_rank.contains(&'k') {
-            available_pieces.retain(|&p| p != 'k');
-        }
-
-        // Only one queen
-        if black_back_rank.contains(&'q') {
-            available_pieces.retain(|&p| p != 'q');
-        }
-
-        // King should be between 2 rooks
-        if black_back_rank.contains(&'r') && !black_back_rank.contains(&'k') {
-            available_pieces.retain(|&p| p != 'r');
-        }
-
-        // Bishops should be on opposite colors
-        if let Some(bishop_index) = black_back_rank.iter().position(|&p| p == 'b') {
-            if i % 2 == bishop_index % 2 {
-                available_pieces.retain(|&p| p != 'b');
-            }
-        }
-
-        let random_piece_index = rng.random_range(..available_pieces.len());
-
-        let random_piece = available_pieces[random_piece_index];
-
-        black_back_rank[i] = random_piece;
+        file.write_all(format!("{}\n", fen).as_bytes())?;
     }
 
-    let black_fen = String::from_iter(black_back_rank);
-
-    let white_fen = black_fen.as_str().to_uppercase();
-
-    let fen = black_fen + inner_fen + &white_fen;
-
-    println!("{}", fen);
+    Ok(())
 }
